d47ea157e4cf36fe405125ef278a0a01ecdb1408
Jah Raphael <jahraphael@yahoo.com>
2017-10-07 08:26:15 -0500

    script to build the addon on OSX

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rfv build
+mkdir -vp build/RaiderIO/db
+cp -v LICENSE *.toc *.lua build/RaiderIO
+cp -v db/*.lua build/RaiderIO/db
+cp -av db/RaiderIO_DB_{EU,KR,TW,US}* build
+
+cd build
+
+rm -f ../RaiderIO_Addon.zip
+zip -r9 ../RaiderIO_Addon.zip .
+
+
