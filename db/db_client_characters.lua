e7dd40c1e92331dfc7caa328d3e4c46a86a22506
Vlad <vladix@gmail.com>
2018-09-03 21:35:15 +0200

    - Pushed the latest data from the Test channel from the client just to keep the git in sync with the test data, for now.

--- db/db_client_characters.lua
+++ db/db_client_characters.lua
@@ -1,6 +1,26 @@
--- __________        .__    .___           .___________   
--- \______   \_____  |__| __| _/___________|   \_____  \  
---  |       _/\__  \ |  |/ __ |/ __ \_  __ \   |/   |   \ 
---  |    |   \ / __ \|  / /_/ \  ___/|  | \/   /    |    \
---  |____|_  /(____  /__\____ |\___  >__|  |___\_______  /
---         \/      \/        \/    \/                  \/ 
+--
+-- RaiderIO Client Data. Generated on 2018-09-03T19:30:47Z
+--
+local _, ns = ...
+ns.CLIENT_CHARACTERS = {
+  ["Voidzone-Ravencrest"] = {
+    ["profile"] = {
+      ["name"] = "Voidzone",
+      ["realm"] = "Ravencrest",
+      ["faction"] = "alliance",
+      ["race"] = 4,
+      ["class"] = 12
+    },
+    ["mythic_keystone"] = {
+      ["all"] = {
+        ["score"] = 0,
+        ["best"] = nil,
+        ["runs"] = {
+
+        }
+      }
+    }
+  }
+}
+
+if RaiderIO then RaiderIO.AddClientCharacters(ns.CLIENT_CHARACTERS) end
