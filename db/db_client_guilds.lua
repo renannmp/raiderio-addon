c143183c7d62cb7e47b46fe08d6c3fb6f09d00b9
Vlad <vladix@gmail.com>
2018-09-03 21:36:04 +0200

    - Okay, minus the client related files, those stay empty.

--- db/db_client_guilds.lua
+++ db/db_client_guilds.lua
@@ -1,34 +1,6 @@
---
--- RaiderIO Client Data. Generated on 2018-09-03T19:30:46Z
---
-local _, ns = ...
-ns.GUILD_BEST_DATA = {
-  ["Eight Days A Week-Auchindoun"] = {
-    ["profile"] = {
-      ["name"] = "Eight Days A Week",
-      ["realm"] = "Auchindoun",
-      ["faction"] = "alliance"
-    },
-    ["season_best"] = {
-
-    },
-    ["weekly_best"] = {
-
-    }
-  },
-  ["Overcharged-Ravencrest"] = {
-    ["profile"] = {
-      ["name"] = "Overcharged",
-      ["realm"] = "Ravencrest",
-      ["faction"] = "alliance"
-    },
-    ["season_best"] = {
-
-    },
-    ["weekly_best"] = {
-
-    }
-  }
-}
-
-if RaiderIO then RaiderIO.AddClientGuilds(ns.GUILD_BEST_DATA) end
+-- __________        .__    .___           .___________   
+-- \______   \_____  |__| __| _/___________|   \_____  \  
+--  |       _/\__  \ |  |/ __ |/ __ \_  __ \   |/   |   \ 
+--  |    |   \ / __ \|  / /_/ \  ___/|  | \/   /    |    \
+--  |____|_  /(____  /__\____ |\___  >__|  |___\_______  /
+--         \/      \/        \/    \/                  \/ 
