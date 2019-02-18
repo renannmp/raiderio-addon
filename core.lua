5938e7fbd99bfdc0e0db95a0706aba1de6a9a51a
Jah Raphael <jahraphael@yahoo.com>
2019-02-17 22:36:10 -0600

    disable client enhancements for now, until out of alpha

--- core.lua
+++ core.lua
@@ -975,7 +975,7 @@ do
 
 			-- if character exists in the clientCharacters list then override some data with higher precision
 			-- TODO: only do this if the clientCharacters data isn't too old compared to regular addon date?
-			if ns.CLIENT_CHARACTERS and ns.addonConfig.enableClientEnhancements then
+			if false and ns.CLIENT_CHARACTERS and ns.addonConfig.enableClientEnhancements then
 				local nameAndRealm = name .. "-" .. realm
 				local clientData = ns.CLIENT_CHARACTERS[nameAndRealm]
 
