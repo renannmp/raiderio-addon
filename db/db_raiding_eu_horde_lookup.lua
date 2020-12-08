e5c2cf6bd1f4db2cdcde2369f6c9566fa761487f
Jah Raphael <jahraphael@yahoo.com>
2020-12-08 06:03:05 +0000

    [Raider.IO] Database Refresh

--- db/db_raiding_eu_horde_lookup.lua
+++ db/db_raiding_eu_horde_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2020 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=2,region="eu",faction=2,date="2020-12-07T06:01:33Z",currentRaid={["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10},previousRaid=nil,lookup2={}}
+local provider={name=...,data=2,region="eu",faction=2,date="2020-12-08T06:01:31Z",currentRaid={["name"]="Castle Nathria",["shortName"]="CN",["bossCount"]=10},previousRaid=nil,lookup2={}}
 local F
 
 
--- db/db_us_alliance_characters.lua
+++ db/db_us_alliance_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2020 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=1,region="us",faction=1,date="2020-12-07T06:00:26Z",currentSeasonId=0,numCharacters=0,db1={}}
+local provider={name=...,data=1,region="us",faction=1,date="2020-12-08T06:00:30Z",currentSeasonId=0,numCharacters=0,db1={}}
 local F
 
 
